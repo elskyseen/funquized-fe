a18ab4179550c2370104a559bb0a01b928eac334
hlkgt <haihaioell@gmail.com>
2024-06-21 14:07:50 +0700

    fix: delete isLogin cookie

--- src/utils/setCookie.ts
+++ src/utils/setCookie.ts
@@ -3,5 +3,4 @@ import { jwtDecode } from "jwt-decode";
 export const setCookie = (token: string): void => {
   const { exp } = jwtDecode(token);
   document.cookie = `iat=${exp};SameSite=None;`;
-  document.cookie = `isLogin=true;SameSite=None;`;
 };
