1d96076afd335aaab8579a163c31b1a3dc76a85f
hlkgt <haihaioell@gmail.com>
2024-06-21 21:19:44 +0700

    create cookie

--- src/utils/setCookie.ts
+++ src/utils/setCookie.ts
@@ -2,6 +2,6 @@ import { jwtDecode } from "jwt-decode";
 
 export const setCookie = (token: string): void => {
   const { exp } = jwtDecode(token);
-  document.cookie = `iat=${exp};SameSite=None;`;
-  document.cookie = `isLogin=true;SameSite=None;`;
+  document.cookie = `iat=${exp};SameSite=None;path=/`;
+  document.cookie = `isLogin=true;SameSite=None;path=/`;
 };
